45065924f3af0da9619c4e5071f19763468a139c
n2rajo00 <128301054+n2rajo00@users.noreply.github.com>
2024-10-01 13:45:42 +0300

    First commit

---
+++ components/Gameboard.js
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import Header from './Header';
+import Footer from './Footer';
+import styles from "../style/style";
+
+export default Gameboard = ({navigation}) => {
+    return(
+        <>
+            <Header />
+            <View>
+                <Text>Gameboard here</Text>
+            </View>
+            <Footer />
+        </>
+    )
+}
